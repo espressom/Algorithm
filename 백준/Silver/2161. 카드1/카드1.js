6e88969162df5e4fba3232a80ba5d27cf16a9103
espressom <45090300+espressom@users.noreply.github.com>
2022-07-14 20:30:55 +0900

    [Silver V] Title: 카드1, Time: 128 ms, Memory: 9772 KB -BaekjoonHub

---
+++ 백준/Silver/2161. 카드1/카드1.js
@@ -0,0 +1,58 @@
+const fs = require("fs");
+const stdin = (
+  process.platform === "linux" ? fs.readFileSync("/dev/stdin").toString() : `7`
+).split("\n");
+
+const input = (() => {
+  let line = 0;
+  return () => stdin[line++];
+})();
+
+const n = +input();
+
+let arr = Array.from(Array(n), (_, idx) => {
+  return idx + 1;
+});
+
+class Queue {
+  constructor(arr) {
+    this.queue = arr;
+    this.front = 0;
+    this.rear = arr.length;
+  }
+
+  enqueue(val) {
+    this.queue[this.rear++] = val;
+  }
+
+  dequeue() {
+    const val = this.queue[this.front];
+    delete this.queue[this.front];
+    this.front += 1;
+    return val;
+  }
+
+  peek() {
+    return this.queue[this.front];
+  }
+
+  size() {
+    return this.rear - this.front;
+  }
+}
+
+const q = new Queue(arr);
+
+let flag = true;
+let res = [];
+while (q.size() > 0) {
+  if (flag) {
+    res.push(q.dequeue());
+    flag = false;
+  } else {
+    q.enqueue(q.dequeue());
+    flag = true;
+  }
+}
+
+console.log(res.join(" "));
